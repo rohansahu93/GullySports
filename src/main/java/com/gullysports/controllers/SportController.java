1fed3bc7cda68ee1f829e3f01c89c1dabbdb596c
Nitesh Sharma <nitesh.sharma@intimetec.americas>
2018-01-23 10:05:21 +0530

    Refactoring

--- src/main/java/com/gullysports/controllers/SportController.java
+++ src/main/java/com/gullysports/controllers/SportController.java
@@ -3,10 +3,9 @@ package com.gullysports.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
 
-import com.gullysports.models.Sport;
-import com.gullysports.services.SportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
+import com.gullysports.models.Sport;
+import com.gullysports.services.SportService;
 
 /**
  * This class is responsible for exposing REST APis for sports.
@@ -28,8 +26,7 @@ public class SportController {
     /**
      * Spring will initialize ProjectService object.
      */
-    @Autowired
-    private SportService sportService;
+    @Autowired private SportService sportService;
 
     /**
      * Rest Interface for fetching all the sports.
@@ -39,7 +36,9 @@ public class SportController {
      */
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public List<Sport> getAllSports() throws Exception {
+    public List<Sport> getAllSports()
+        throws Exception {
+
         return sportService.getAllSports();
     }
 
@@ -50,7 +49,8 @@ public class SportController {
      * @return added sport details
      */
     @RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
-    public Sport addSport(@RequestBody @Valid Sport sport) throws Exception{
+    public Sport addSport(@RequestBody @Valid Sport sport)
+        throws Exception {
 
         return sportService.addSport(sport);
     }
@@ -63,7 +63,9 @@ public class SportController {
      */
     @RequestMapping(value = "{sportId}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Sport getSport(@PathVariable("sportId") String sportId) throws Exception {
+    public Sport getSport(@PathVariable("sportId") String sportId)
+        throws Exception {
+
         return sportService.getSport(sportId);
     }
 
@@ -76,7 +78,9 @@ public class SportController {
 
     @RequestMapping(value = "", method = RequestMethod.PUT, produces = "application/json")
     @ResponseBody
-    public Sport updateSport(@RequestBody @Valid Sport sport) throws Exception {
+    public Sport updateSport(@RequestBody @Valid Sport sport)
+        throws Exception {
+
         return sportService.updateSport(sport);
     }
 
@@ -89,7 +93,9 @@ public class SportController {
 
     @RequestMapping(value = "{sportId}", method = RequestMethod.DELETE, produces = "application/json")
     @ResponseBody
-    public void deleteSport(@PathVariable("sportId") String sportId) throws Exception {
+    public void deleteSport(@PathVariable("sportId") String sportId)
+        throws Exception {
+
         sportService.deleteSport(sportId);
     }
 }
